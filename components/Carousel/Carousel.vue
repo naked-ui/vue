4f1434e51727fc4c81dbd0466fbc8fb77d182ce5
Aleksander Grygier <alek@newfantastic.com>
2020-12-21 00:46:05 +0100

    refactor: Update project setup with Vue CLI created app

---
+++ components/Carousel/Carousel.vue
@@ -0,0 +1,60 @@
+<template>
+  <div
+    class="carousel"
+    :paginationEnabled="paginationEnabled"
+  >
+    <div class="carousel__viewport">
+      <slot />
+    </div>
+    <aside
+      v-if="paginationEnabled"
+      class="carousel__pagination"
+    >
+      <ol class="carousel__pagination-list">
+        <li
+          v-for="(item, index) in paginationItems"
+          :key="index"
+          class="carousel__pagination-item">
+          <a
+            :href="`#${carouselKey}--${index}`"
+            class="carousel__pagination-button"
+          >Go to slide {{ index }}</a>
+        </li>
+      </ol>
+    </aside>
+  </div>
+</template>
+
+<script>
+import carouselNavigation from './carouselNavigation'
+
+export default {
+  name: 'Carousel',
+  mixins: [
+    carouselNavigation
+  ],
+  props: { 
+    paginationItems: {
+      type: Array,
+      required: true
+    },
+    paginationEnabled: {
+      type: Boolean,
+      default: true
+    },
+    carouselKey: {
+      type: String,
+      required: true,
+      default: 'slide'
+    }
+  },
+  methods: {
+    prevSlide (index) {
+      return index - 1
+    },
+    nextSlide (index) {
+      return index + 1
+    }
+  }
+}
+</script>
---
+++ components/Carousel/CarouselItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <li
+    class="carousel__slide"
+    :tabindex="slideIndex"
+    :id="`${carouselKey}--${slideIndex}`"
+  >
+    <div class="carousel__snapper">
+      <a
+        :href="`#${carouselKey}--${prevSlide(slideIndex)}`"
+        class="carousel__prev"
+      />
+      <a
+        :href="`#${carouselKey}--${nextSlide(slideIndex)}`"
+        class="carousel__next"
+      />
+    </div>
+    <slot />
+  </li>
+</template>
+
+<script>
+import carouselNavigation from './carouselNavigation'
+
+export default {
+  name: 'CarouselItem',
+  mixins: [
+    carouselNavigation
+  ],
+  props: {
+    slideIndex: {
+      type: String | Number,
+      required: true
+    },
+    carouselKey: {
+      type: String,
+      default: 'slide'
+    }
+  }
+}
+</script>
