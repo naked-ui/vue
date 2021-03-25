20bb74dbf880c5fdbae5513d11d46cd8b4bb21ca
Aleksander Grygier <alek@newfantastic.com>
2021-03-25 18:06:06 +0100

    feat: Color Input PoC

--- src/components/nColorInput/nColorInput.stories.js
+++ src/components/nColorInput/nColorInput.stories.js
@@ -1,17 +1,27 @@
 import nColorInput from './nColorInput.vue'
 
 export default {
-    title: 'Form/Input/ColorInput',
-    component: nColorInput
+  title: 'Form/Input/ColorInput',
+  component: nColorInput
 }
 
 const Template = (args, { argTypes }) => ({
-    components: { nColorInput },
-    props: Object.keys(argTypes),
-    template: '<nColorInput v-bind="$props" />',
+  components: { nColorInput },
+  props: Object.keys(argTypes),
+  template: `
+    <div>
+      <nColorInput
+        v-bind="$props"
+      />
+    </div>
+  `,
+  data: () => ({
+    color: ''
+  })
 });
 
 export const Default = Template.bind({});
 Default.args = {
-    url: 'https://naked-ui.org/'
+  name: 'color-input',
+  id: 'color-input'
 }
--- src/components/nColorInput/nColorInput.vue
+++ src/components/nColorInput/nColorInput.vue
@@ -2,17 +2,76 @@
   <div
     class="color-input"
     :nui-namespace="uiNamespace"
+    :style="`
+      --gap: ${isNaN(gap) ? gap : gap + 'px'};
+      --height: ${isNaN(height) ? height : height + 'px'};
+      --width: ${isNaN(width) ? width : width + 'px'};
+      --padding: ${padding};
+      --outline-width: ${isNaN(outlineWidth) ? outlineWidth : outlineWidth + 'px'};
+      --color-invalid: ${colorInvalid};
+      --color-valid: ${colorValid};
+    `"
   >
-    <input type="color">
+    <label
+      class="color-input__label"
+      :disabled="disabled"
+      :for="id"
+    >
+      {{ label }}
+    </label>
+    <input
+      type="color"
+      v-model="formFieldValue"
+      @change="printColorValue($event); validate();"
+      :autofocus="autofocus"
+      :disabled="disabled"
+      :id="id"
+      :name="name"
+      :placeholder="placeholder"
+      :readonly="readonly"
+      :required="required"
+      :title="title"
+    >
+    <input
+      pattern="#[a-fA-F\d]+"
+      type="text"
+      v-model="formFieldValue"
+      @keyup="updateColorValue($event)"
+    >
+    <div
+      class="color-input__alerts"
+    >
+      <span
+        v-for="(message, index) in validationMessages"
+        :key="index"
+        :class="[
+          'color-input__alerts-item'
+        ]"
+        :style="`
+          --color: ${message.color}
+        `"
+        v-html="message.content"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import namespaceMixin from '../../utils/namespace'
+import formField from '../../utils/formField'
 
 export default {
-  mixins: [namespaceMixin],
-  name: 'nColorInput'
+  mixins: [ namespaceMixin, formField ],
+  methods: {
+    updateColorValue (e) {
+    	e.target.value = this.formFieldValue
+    },
+    printColorValue (e) {
+    	this.formFieldValue = e.target.value
+    }
+  }
 }
 
 </script>
+
+<style lang="scss" src="./nColorInput.scss" />
