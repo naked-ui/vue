ce558dd274c3e1367ba8ec0cfe57692e25bc1eac
Aleksander Grygier <alek@newfantastic.com>
2021-03-27 17:42:03 +0100

    feat: Create TimeInput PoC

--- src/components/nTimeInput/nTimeInput.stories.js
+++ src/components/nTimeInput/nTimeInput.stories.js
@@ -1,17 +1,84 @@
 import nTimeInput from './nTimeInput.vue'
 
 export default {
-    title: 'Form/Input/TimeInput',
-    component: nTimeInput
+  title: 'Form/Input/TimeInput',
+  component: nTimeInput,
+  argTypes: {
+    disabled: {
+      control: 'boolean'
+    },
+    required: {
+      control: 'boolean'
+    },
+    readonly: {
+      control: 'boolean'
+    },
+    counterEnabled: {
+      control: 'boolean',
+      defaultValue: true
+    },
+    gap: {
+      control: {
+        type: 'range',
+        min: 0,
+        max: 80
+      }
+    },
+    height: {
+      control: {
+        type: 'range',
+        min: 0,
+        max: 80
+      }
+    },
+    minlength: {
+      control: {
+        type: 'range',
+        min: 3,
+        max: 80
+      }
+    },
+    maxlength: {
+      control: {
+        type: 'range',
+        min: 0,
+        max: 80
+      }
+    },
+    autocorrect: {
+      control: {
+        type: 'select',
+        options: [ 'on', 'off' ]
+      }
+    },
+  }
 }
 
 const Template = (args, { argTypes }) => ({
-    components: { nTimeInput },
-    props: Object.keys(argTypes),
-    template: '<nTimeInput v-bind="$props" />',
+  components: { nTimeInput },
+  props: Object.keys(argTypes),
+  template: `
+    <nTimeInput v-bind="$props" />
+  `,
 });
 
 export const Default = Template.bind({});
 Default.args = {
-    url: 'https://naked-ui.org/'
+  id: 'Time-input',
+  label: 'Time input label',
+  placeholder: 'Time input placeholder',
+  gap: 12,
+  height: 48,
+  width: 'auto',
+  padding: '0 12px',
+  autofocus: false,
+  disabled: false,
+  maxlength: 24,
+  minlength: 1,
+  outlineWidth: '2px',
+  validationMessages: [
+    '&cross; Validation message'
+  ],
+  counterEnabled: true,
+  validInputAlertEnabled: true
 }
--- src/components/nTimeInput/nTimeInput.vue
+++ src/components/nTimeInput/nTimeInput.vue
@@ -1,18 +1,75 @@
 <template>
   <div
-    class="date-input"
+    class="form-field"
     :nui-namespace="uiNamespace"
+    :style="`
+      --gap: ${isNaN(gap) ? gap : gap + 'px'};
+      --height: ${isNaN(height) ? height : height + 'px'};
+      --width: ${isNaN(width) ? width : width + 'px'};
+      --padding: ${padding};
+      --outline-width: ${isNaN(outlineWidth) ? outlineWidth : outlineWidth + 'px'};
+      --color-invalid: ${colorInvalid};
+      --color-valid: ${colorValid};
+    `"
   >
-    <input type="time">
+    <label
+      class="form-field__label"
+      :disabled="disabled"
+      :for="id"
+    >
+      {{ label }}
+    </label>
+    <input
+      type="time"
+      v-model="formFieldValue"
+      @keyup="countCharacters(); validate()"
+      :autofocus="autofocus"
+      :disabled="disabled"
+      :id="id"
+      :max="max"
+      :min="min"
+      :name="name"
+      :placeholder="placeholder"
+      :readonly="readonly"
+      :required="required"
+      :title="title"
+      class="form-field__input-box"
+    >
+    <div
+      class="form-field__alerts"
+    >
+      <span
+        v-for="(message, index) in validationMessages"
+        :key="index"
+        :class="[
+          'form-field__alerts-item'
+        ]"
+        :style="`
+          --color: ${message.color}
+        `"
+        v-html="message.content"
+      />
+    </div>
+    <div
+      class="form-field__counter"
+      v-if="maxlength && counterEnabled"
+    >
+      <span>{{ totalCharacters }}</span>
+      <slot name="counter-separator">/</slot>
+      <span>{{ maxlength }}</span>
+    </div>
   </div>
 </template>
 
 <script>
 import namespaceMixin from '../../utils/namespace'
+import formField from '../../utils/formField'
 
 export default {
-  mixins: [namespaceMixin],
-  name: 'nDateInput'
+  mixins: [ namespaceMixin, formField ],
+  name: 'nTextInput'
 }
 
 </script>
+
+<style lang="scss" src="./nTimeInput.scss" />
