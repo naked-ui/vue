5c696894365b0651c7f9fd3d9eb910d53f91c32b
Aleksander Grygier <alek@newfantastic.com>
2021-03-27 17:40:42 +0100

    feat: Date Input PoC

--- src/components/nDateInput/nDateInput.stories.js
+++ src/components/nDateInput/nDateInput.stories.js
@@ -1,17 +1,84 @@
 import nDateInput from './nDateInput.vue'
 
 export default {
-    title: 'Form/Input/DateInput',
-    component: nDateInput
+  title: 'Form/Input/DateInput',
+  component: nDateInput,
+  argTypes: {
+    disabled: {
+      control: 'boolean'
+    },
+    required: {
+      control: 'boolean'
+    },
+    readonly: {
+      control: 'boolean'
+    },
+    counterEnabled: {
+      control: 'boolean',
+      defaultValue: true
+    },
+    gap: {
+      control: {
+        type: 'range',
+        min: 0,
+        max: 80
+      }
+    },
+    height: {
+      control: {
+        type: 'range',
+        min: 0,
+        max: 80
+      }
+    },
+    minlength: {
+      control: {
+        type: 'range',
+        min: 3,
+        max: 80
+      }
+    },
+    maxlength: {
+      control: {
+        type: 'range',
+        min: 0,
+        max: 80
+      }
+    },
+    autocorrect: {
+      control: {
+        type: 'select',
+        options: [ 'on', 'off' ]
+      }
+    },
+  }
 }
 
 const Template = (args, { argTypes }) => ({
-    components: { nDateInput },
-    props: Object.keys(argTypes),
-    template: '<nDateInput v-bind="$props" />',
+  components: { nDateInput },
+  props: Object.keys(argTypes),
+  template: `
+    <nDateInput v-bind="$props" />
+  `,
 });
 
 export const Default = Template.bind({});
 Default.args = {
-    url: 'https://naked-ui.org/'
+  id: 'Date-input',
+  label: 'Date input label',
+  placeholder: 'Date input placeholder',
+  gap: 12,
+  height: 48,
+  width: 'auto',
+  padding: '0 12px',
+  autofocus: false,
+  disabled: false,
+  maxlength: 24,
+  minlength: 1,
+  outlineWidth: '2px',
+  validationMessages: [
+    '&cross; Validation message'
+  ],
+  counterEnabled: true,
+  validInputAlertEnabled: true
 }
