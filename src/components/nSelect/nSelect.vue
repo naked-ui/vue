4da8c783e331f0ea6e2f6411b3c5d432a25152b7
ibockowsky <igor@newfantastic.com>
2021-04-06 14:13:13 +0200

    feat: refactor select component

---
+++ src/components/nSelect/nSelect.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="n-select">
+    <span class="n-select--label" :id="uiElementID">{{ label }}</span>
+    <div class="n-select--wrapper">
+      <select v-on="listeners" class="n-select__native" :aria-labelledby="uiElementID">
+        <option value="" selected disabled>{{ defaultPlaceholder }}</option>
+        <option v-for="option in options" :value="option.value" :key="option.value">{{ option.text }}</option>
+      </select>
+      <div class="n-select__custom" @click="isHidden = !isHidden" :class="{'active': !isHidden}" :aria-hidden="isHidden">
+        <div class="n-select__custom--placeholder">{{ defaultPlaceholder }}</div>
+        <div class="n-select__custom--options">
+          <div
+            v-for="option in options"
+            @click="handleCustomSelect(option.value)"
+            class="n-select__custom--option"
+            :class="{'selected': selected && selected.value === option.value}"
+            :data-value="option.value"
+            :key="option.value">
+              {{ option.text }}
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import uuidMixin from '../../utils/uuid'
+
+export default {
+  name: 'nSelect',
+  inheritAttrs: false,
+  mixins: [uuidMixin],
+  props: {
+    value: {
+      required: true
+    },
+    label: {
+      type: String,
+      required: false,
+      default: undefined
+    },
+    options: {
+      type: Array,
+      required: true
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    }
+  },
+  data: () => ({
+    isHidden: true,
+    selected: null
+  }),
+  computed: {
+    listeners () {
+      return {
+        ...this.$listeners,
+        input: e => this.$emit('input', this.findSelected(e.target.value)),
+        change: e => this.$emit('change', this.findSelected(e.target.value))
+      }
+    },
+    defaultPlaceholder () {
+      return this.selected ?
+              this.selected.text :
+              this.placeholder
+    }
+  },
+  methods: {
+    findSelected(value) {
+      const options = [...this.options]
+
+      this.selected = options.find(option => option.value === value)
+      return this.selected
+    },
+    handleCustomSelect(value) {
+      this.$emit('input', this.findSelected(value))
+      console.log(this.selected)
+    }
+  }
+}
+</script>
+
+<style lang="scss" src="./nSelectInput.scss" />
--- src/components/nSelect/nSelectInput.stories.js
+++ src/components/nSelect/nSelectInput.stories.js
@@ -1,34 +1,42 @@
-import nSelectInput from './nSelectInput'
-import nSelectOption from './nSelectOption'
-import nSelectOptgroup from './nSelectOptgroup'
+import nSelect from './nSelect'
 
 export default {
     title: 'Form/Input/Select',
-    component: nSelectInput,
-    argTypes: {
-        textColor: { control: { type: 'color' } },
-        bgSelectColor: { control: { type: 'color' } },
-        bgOptionColor: { control: { type: 'color' } },
-        bgOptgroupColor: { control: { type: 'color' } },
-        outlineColor: { control: { type: 'color' } }
-    }
+    component: nSelect,
 }
 
 const Template = (args, {argTypes}) => ({
-    components: { nSelectInput, nSelectOption, nSelectOptgroup },
+    components: { nSelect },
     props: Object.keys(argTypes),
     data: () => ({
-        options_one: ['Option 1', 'Option 2', 'Option 3'],
-        options_two: ['Option 4', 'Option 5', 'Option 6']
+        selectValue: null
     }),
     template: `
-        <nSelectInput v-bind="$props">
-            <nSelectOptgroup label="Options">
-                <nSelectOption v-for="(option, $index) in options_one" :key="$index">{{ option }}</nSelectOption>
-            </nSelectOptgroup>
-            <nSelectOption v-for="(option, $index) in options_two" :key="$index">{{ option }}</nSelectOption>
-        </nSelectInput>
+    <div class="width: 100%;">
+        <div class="width: 100%;"><nSelect v-bind="$props" v-model="selectValue" /></div>
+        <div style="margin-top: 50px;">{{ selectValue }}</div>
+    </div>
     `
 })
 
 export const Default = Template.bind({})
+Default.args = {
+    options: [
+        {
+            text: 'Option 1',
+            value: 'Option 1'
+        },
+        {
+            text: 'Option 2',
+            value: 'Option 2'
+        },
+        {
+            text: 'Option 3',
+            value: 'Option 3'
+        },
+        {
+            text: 'Option 4',
+            value: 'Option 4'
+        },
+    ]
+}
