4cf5cb02956e69ee7903e6397da39e3992549bcd
Razz21 <razzrazz@wp.pl>
2021-04-11 15:45:22 +0200

    feat: custom validation color

--- src/components/nCheckboxInput/nCheckboxInput.stories.js
+++ src/components/nCheckboxInput/nCheckboxInput.stories.js
@@ -46,7 +46,10 @@ Default.args = {
   id: 'checkbox-input',
   name: 'checkbox-input',
   label: 'Label text',
-  customMessages: { valueMissing: 'Value is required!' },
+  customMessages: { valueMissing: {
+    text: 'Value is required!',
+    color: 'green'
+  } },
   gap: 8,
   validationEnabled: true,
   required: true
