9fe3cf90a2650fc5a64ae7f8bd79186443d44bf9
Aleksander Grygier <alek@newfantastic.com>
2021-04-26 13:04:05 +0200

    refactor: Naming update

--- src/components/nAccordion/nAccordionItem.vue
+++ src/components/nAccordion/nAccordionItem.vue
@@ -1,18 +1,38 @@
 <template>
-  <li :class="`${componentClasses}--item`">
-    <div :class="[`${componentClasses}--item__content`, { 'open': open }]" @click="open = !open">
-      <slot name="title" />
-      <nIcon class="" :class="[`${componentClasses}--item__arrow`, { 'open': open }]" :size="12">
-        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 256 256" style="enable-background:new 0 0 256 256;" xml:space="preserve">
-        <g>
+  <li :class="`${componentClasses}-item`">
+    <div
+      :class="[`${componentClasses}-item__inner`, { open: open }]"
+      @click="open = !open"
+    >
+      <span :class="`${componentClasses}-item__title`">
+        <slot name="title" />
+      </span>
+      <nIcon
+        class=""
+        :class="[`${componentClasses}-item__icon`, { open: open }]"
+        :size="12"
+      >
+        <svg
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          x="0px"
+          y="0px"
+          viewBox="0 0 256 256"
+          style="enable-background: new 0 0 256 256"
+          xml:space="preserve"
+        >
           <g>
-            <polygon points="225.813,48.907 128,146.72 30.187,48.907 0,79.093 128,207.093 256,79.093"/>
+            <g>
+              <polygon
+                points="225.813,48.907 128,146.72 30.187,48.907 0,79.093 128,207.093 256,79.093"
+              />
+            </g>
           </g>
-        </g>
         </svg>
       </nIcon>
     </div>
-    <div :class="`${componentClasses}--item__text`" v-show="open">
+    <div :class="`${componentClasses}-item__content`" v-show="open">
       <slot name="text" />
     </div>
   </li>
@@ -28,14 +48,12 @@ export default {
     baseClassname: {
       type: String,
       default: 'n-accordion'
-    },
+    }
   },
   computed: {
-    componentClasses () {
-      return [
-        this.baseClassname
-      ]
-    },
+    componentClasses() {
+      return [this.baseClassname]
+    }
   },
   data: () => ({
     open: false
