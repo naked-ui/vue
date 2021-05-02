c5f0a39363f5fb62c9678cb8584037f855b66efb
Aleksander Grygier <alek@newfantastic.com>
2021-05-02 17:28:44 +0200

    refactor: Styling & naming cleanup

--- src/components/nSelect/nSelectInput.stories.js
+++ src/components/nSelect/nSelectInput.stories.js
@@ -4,17 +4,11 @@ export default {
     title: 'Form/Select',
     component: nSelect,
     argTypes: {
-        textColor: { control: { type: 'color' } },
-        textHoverColor: { control: { type: 'color' } },
+        color: { control: { type: 'color' } },
         backgroundColor: { control: { type: 'color' } },
         borderColor: { control: { type: 'color' } },
-        borderHoverColor: { control: { type: 'color' } },
         optionHoverColor: { control: { type: 'color' } },
-        optionBorderColor: { control: { type: 'color' } },
-        optionSelectedColor: { control: { type: 'color' } },
-        chipBackgroundColor: { control: { type: 'color' } },
-        chipHoverColor: { control: { type: 'color' } },
-        chipBorderColor: { control: { type: 'color' } }
+        // optionSelectedColor: { control: { type: 'color' } },
     }
 }
 
