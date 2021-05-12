ab50359f38e5b40ecf6f6ad7530f815c016970c5
ibockowsky <igor@newfantastic.com>
2021-05-12 09:36:04 +0200

    refactor: better uuid constructor

--- src/utils/uuid.js
+++ src/utils/uuid.js
@@ -3,8 +3,8 @@ const uuid = () => Math.random().toString(36).substr(2, 9)
 export default {
     computed: {
       uiElementID () {
-        if (this.customUiNamespace) {
-          return `${this.customUiNamespace}_${uuid()}`
+        if (this.id) {
+          return `${this.id}_${uuid()}`
         } else return `nui_${uuid()}`
       }
     }
