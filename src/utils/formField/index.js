dd7bb6baec3dbe720228d1331a5b1713a2c84576
ibockowsky <igor@newfantastic.com>
2021-05-25 11:38:11 +0200

    refactor: code clean up

--- src/utils/formField/index.js
+++ src/utils/formField/index.js
@@ -1,41 +1,43 @@
 import styleVariables from '../styleVariables'
 import formFieldProps from '../props/formFieldProps'
 import formFieldCounter from './helpers/formFieldCounter'
+import formFieldMaxLength from './helpers/formFieldMaxLength'
 import formFieldValidations from './helpers/formFieldValidations'
 import {
-  height,
-  width,
   gap,
-  backgroundColor,
+  width,
+  height,
   padding,
   colorValid,
-  colorInvalid,
   borderWidth,
   borderColor,
   borderStyle,
   borderRadius,
+  colorInvalid,
+  backgroundColor,
 } from '../styleVariables/helpers/variables'
 
 const defaultStyleVariables = [
   gap,
-  height,
   width,
-  backgroundColor,
+  height,
   padding,
   colorValid,
-  colorInvalid,
   borderWidth,
   borderColor,
   borderStyle,
   borderRadius,
+  colorInvalid,
+  backgroundColor,
 ]
 
 export const formField = (customStyleVariables = []) => {
   return {
     mixins: [
       formFieldProps,
-      formFieldValidations,
       formFieldCounter,
+      formFieldMaxLength,
+      formFieldValidations,
       styleVariables(defaultStyleVariables, customStyleVariables)
     ],
   }
