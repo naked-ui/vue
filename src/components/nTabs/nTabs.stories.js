af4bb5fbb7a5a1527c4fce4112c2793ef1be41b1
Aleksander Grygier <alek@newfantastic.com>
2021-05-28 10:43:02 +0200

    docs: Tabs story update

--- src/components/nTabs/nTabs.stories.js
+++ src/components/nTabs/nTabs.stories.js
@@ -8,32 +8,17 @@ export default {
     controlPadding: { control: 'text' },
     tabPadding: { control: 'text' },
     width: { control: 'text' },
-    vertical: { control: 'boolean' }
+    vertical: { control: 'boolean' },
+    tabs: { control: 'array' }
   }
 }
 
 const DefaultTemplate = (args, { argTypes }) => ({
   components: { nTabs, nTab },
   props: Object.keys(argTypes),
-  data: () => ({
-    slotItems: [
-      {
-        title: 'First tab',
-        content: 'Cupidatat id sit ea nulla culpa duis voluptate pariatur. Voluptate ea nulla incididunt excepteur et aliqua. Sint voluptate officia quis aliquip. Laboris culpa pariatur exercitation ex nostrud nostrud. Quis cillum velit veniam incididunt sint fugiat nisi.'
-      },
-      {
-        title: 'Second tab',
-        content: 'Laboris ipsum et et cupidatat adipisicing. Voluptate exercitation eiusmod laborum nostrud dolor ad ex. Nostrud duis proident in dolor aute tempor quis velit deserunt eu fugiat pariatur sunt.'
-      },
-      {
-        title: 'Third tab',
-        content: 'Lorem sunt aliquip exercitation elit elit deserunt enim incididunt reprehenderit qui. Qui voluptate id quis reprehenderit eu dolor sint magna exercitation eiusmod laborum consectetur anim. Minim adipisicing culpa qui officia ex nisi culpa velit tempor minim dolore aliquip et incididunt. Enim tempor aute dolor ullamco.'
-      },
-    ]
-  }),
   template: `
     <nTabs v-bind="$props">
-        <nTab v-for="(item, $index) in slotItems" :key="$index" :label="item.title">{{ item.content }}</nTab>
+        <nTab v-for="(item, $index) in tabs" :key="$index" :label="item.title">{{ item.content }}</nTab>
     </nTabs>
   `
 })
@@ -43,5 +28,19 @@ export const Default = DefaultTemplate.bind({})
 Default.args = {
   width: '480px',
   controlPadding: '8px',
-  tabPadding: '12px 0'
+  tabPadding: '12px 0',
+  tabs: [
+    {
+      title: 'First tab',
+      content: 'Cupidatat id sit ea nulla culpa duis voluptate pariatur. Voluptate ea nulla incididunt excepteur et aliqua. Sint voluptate officia quis aliquip. Laboris culpa pariatur exercitation ex nostrud nostrud. Quis cillum velit veniam incididunt sint fugiat nisi.'
+    },
+    {
+      title: 'Second tab',
+      content: 'Laboris ipsum et et cupidatat adipisicing. Voluptate exercitation eiusmod laborum nostrud dolor ad ex. Nostrud duis proident in dolor aute tempor quis velit deserunt eu fugiat pariatur sunt.'
+    },
+    {
+      title: 'Third tab',
+      content: 'Lorem sunt aliquip exercitation elit elit deserunt enim incididunt reprehenderit qui. Qui voluptate id quis reprehenderit eu dolor sint magna exercitation eiusmod laborum consectetur anim. Minim adipisicing culpa qui officia ex nisi culpa velit tempor minim dolore aliquip et incididunt. Enim tempor aute dolor ullamco.'
+    },
+  ]
 }
