0d2607320a99af3ce890180869da6ed13a5ca3e3
ibockowsky <igor@newfantastic.com>
2021-06-02 10:37:03 +0200

    refactor: move validation logic to external files

---
+++ src/components/nCheckboxInput/logic/groupValidationHandler.js
@@ -0,0 +1,68 @@
+import { messages } from '@/utils/validation'
+import nValidationAlerts from '@/utils/components/nValidationAlerts.vue'
+
+export default {
+  components: { nValidationAlerts },
+  provide() {
+    return {
+      checkboxGroup: {
+        required: this.required,
+        readonly: this.readonly,
+        disabled: this.disabled,
+        value: this.value
+      }
+    }
+  },
+  computed: {
+    errorMessage() {
+      if (this.customMessages.hasOwnProperty('valueMissing')) {
+        return this.customMessages['valueMissing']
+      }
+      return messages.valueMissing.default
+    }
+  },
+  methods: {
+    setValidity() {
+      // checkbox(-group) has only one validation state: `required`
+      // custom `setValidity` is triggered by all inputs simultaneously,
+      // but the result can be only one
+      this.validationMessages = [
+        {
+          content: this.errorMessage, // or custom message
+          color: this.colorInvalid
+        }
+      ]
+    },
+    checkValidity() {
+      if (this.required && !this.selectedValue.length) return this.setValidity()
+      this.validationMessages = []
+    },
+    validate(e, value) {
+      // triggered by any checkbox-input child on value change
+      if (this.value.includes(value))
+        this.selectedValue = this.value.filter((v) => v !== value)
+      else this.selectedValue = this.value.concat([value])
+      this.checkValidity()
+      this.$emit('input', this.selectedValue)
+    },
+    onValidate ({ e, value }) {
+      this.validate(e, value)
+    }
+  },
+  mounted() {
+    this.$on('nui:validate-checkbox-group', (e) => {
+      this.onValidate(e)
+    })
+    this.$on('nui:set-validity-checkbox-group', (e) => {
+      this.setValidity(e)
+    })
+  },
+  destroyed() {
+    this.$off('nui:validate-checkbox-group', (e) => {
+      this.onValidate(e)
+    })
+    this.$off('nui:set-validity-checkbox-group', (e) => {
+      this.setValidity(e)
+    })
+  }
+}
---
+++ src/components/nCheckboxInput/logic/validationHandler.js
@@ -0,0 +1,56 @@
+import nValidationAlerts from '@/utils/components/nValidationAlerts.vue'
+
+
+export default {
+  components: { nValidationAlerts },
+  inject: {
+    checkboxGroup: {
+      default: null
+    }
+  },
+  computed: {
+    isDisabled() {
+      return (
+        this.disabled || (!!this.checkboxGroup && this.checkboxGroup.disabled)
+      )
+    },
+    isReadonly() {
+      return (
+        this.readonly || (!!this.checkboxGroup && this.checkboxGroup.readonly)
+      )
+    },
+    isRequired() {
+      return (
+        this.required || (!!this.checkboxGroup && this.checkboxGroup.required)
+      )
+    },
+    isChecked() {
+      if (this.checked) return this.checked
+      else if (this.checkboxGroup) {
+        const { value } = this.checkboxGroup
+        return value.includes(this.value)
+      }
+      return false
+    }
+  },
+  methods: {
+    $setValidity(e) {
+      if (this.checkboxGroup) {
+        this.$parent.$emit('nui:set-validity-checkbox-group', e)
+      } else {
+        this.setValidity(e)
+      }
+    },
+    $validate(e) {
+      if (this.checkboxGroup) {
+        // send value to compare equal values (e.g. checkbox `value` attribute converts numers to string)
+        this.$parent.$emit('nui:validate-checkbox-group', {
+          e,
+          value: this.value
+        })
+      } else {
+        this.validateFormField(e)
+      }
+    }
+  }
+}
