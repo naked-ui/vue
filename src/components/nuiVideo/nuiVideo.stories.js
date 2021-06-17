ef0c5b3e6244b4bbf3274729dc88d4d939b54c27
Aleksander Grygier <alek@newfantastic.com>
2021-06-17 17:07:15 +0200

    feat: Video component PoC

---
+++ src/components/nuiVideo/nuiVideo.stories.js
@@ -0,0 +1,57 @@
+import nuiVideo from './nuiVideo.vue'
+
+export default {
+  title: 'Video',
+  component: nuiVideo,
+  argTypes: {
+    width: {
+      control: 'text',
+      defaultValue: '100%'
+    },
+    height: {
+      control: 'text',
+      defaultValue: '100%'
+    },
+    background: {
+      control: 'text',
+      defaultValue: '#ddd'
+    },
+    maxWidth: {
+      control: 'text'
+    },
+    maxHeight: {
+      control: 'text'
+    },
+    objectFit: {
+      control: {
+        type: 'select',
+        options: ['contain', 'cover']
+      },
+      // defaultValue: 'none'
+    },
+    image: {
+      type: 'object'
+    },
+    images: {
+      type: 'array'
+    }
+  },
+}
+
+export const Default = (args, { argTypes }) => ({
+  components: { nuiVideo },
+  props: Object.keys(argTypes),
+  template: `
+    <nuiVideo
+      v-bind="$props"
+      :image="image"
+    />
+  `
+});
+
+Default.args = {
+  video: {
+    src: 'https://file-examples-com.github.io/uploads/2020/03/file_example_WEBM_1280_3_6MB.webm',
+    type: 'video/webm'
+  }
+}
---
+++ src/components/nuiVideo/nuiVideo.vue
@@ -0,0 +1,73 @@
+<template>
+  <video
+    :class="`${baseClassname}`"
+    :id="uiElementID()"
+    :style="style"
+    loading="lazy"
+    decoding="async"
+    :width="width"
+    :height="height"
+    style="content-visibility: auto"
+    autoplay
+    loop
+  >
+    <source v-bind="video" />
+  </video>
+</template>
+
+<script>
+import uuID from '@/utils/uuid'
+import styleVariables from '@/utils/styleVariables/index'
+import styleProps from '@/utils/props/styleProps'
+import {
+  height,
+  width,
+  maxHeight,
+  maxWidth,
+  background,
+  objectFit
+} from '@/utils/styleVariables/helpers/variables'
+
+const componentStyleVariables = [
+  height,
+  width,
+  maxHeight,
+  maxWidth,
+  background,
+  objectFit
+]
+
+const componentProps = {
+  // UI
+  baseClassname: {
+    type: String,
+    default: 'nui-picture'
+  },
+  loading: {
+    type: String,
+    default: 'lazy'
+  },
+  autoplay: {
+    type: Boolean
+  },
+  loop: {
+    type: Boolean
+  },
+  // Data
+  video: {
+    type: Object,
+    default() {
+      return null
+    }
+  },
+  ...styleProps
+}
+
+export default {
+  name: 'nuiVideo',
+  mixins: [uuID, styleVariables(componentStyleVariables)],
+  props: componentProps
+}
+</script>
+
+<style lang="scss" src="./nuiVideo.scss" />
